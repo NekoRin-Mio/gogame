561249c64ecabe2b1608bbcbef3ead58291d01e2
Tsuki <89490849+NekoRin-Mio@users.noreply.github.com>
2026-08-15 22:10:57 -0600

    Add app/src/main/java/com/gogame/ui/Shape.kt

---
+++ app/src/main/java/com/gogame/ui/Shape.kt
@@ -0,0 +1,16 @@
+package com.gogame.ui
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ * SPEC 2025 形状
+ */
+val SpecShapes = Shapes(
+    extraSmall = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(12.dp),
+    large = RoundedCornerShape(16.dp),
+    extraLarge = RoundedCornerShape(28.dp)
+)
